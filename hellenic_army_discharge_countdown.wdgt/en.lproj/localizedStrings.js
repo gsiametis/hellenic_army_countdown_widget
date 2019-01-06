a637b65fac1057a5ad3f48fc9c9a1f9023a4049d
Georgios Siametis <gsiametis@gmail.com>
2019-01-06 17:17:28 +0100

    Begin

---
+++ hellenic_army_discharge_countdown.wdgt/en.lproj/localizedStrings.js
@@ -0,0 +1,11 @@
+var localizedStrings = new Object;
+
+localizedStrings["Countdown to %s"] = "Countdown to %s";
+localizedStrings["Event Label"] = unescape('%u0397%u03BC%u03B5%u03C1%u03BF%u03BC%u03B7%u03BD%u03AF%u03B1 %u03B1%u03C0%u03BF%u03BB%u03CD%u03C3%u03B5%u03C9%u03C2');
+localizedStrings["Done"] = "Done";
+
+localizedStrings["DAYS"]  = unescape('%u03BA%u03B1%u03B9 %u03C3%u03AE%u03BC%u03B5%u03C1%u03B1%u2026');
+localizedStrings["HOURS"] = "HOURS";
+localizedStrings["MIN"]   = "MIN";
+localizedStrings["SEC"]   = "SEC";
+localizedStrings[unescape('%3Ca href=%22http://www.macgiorgosgr.com%22%3Emacgiorgosgr.com%3C/a%3E')] = unescape('%3Ca href=%22http://www.macgiorgosgr.com%22%3Emacgiorgosgr.com%3C/a%3E');
